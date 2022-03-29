e4e4b53c31b47272b38c2f53d3e3be9c5638f7de
SolohagaLukas <solohagalukas@gmail.com>
2022-03-29 02:00:05 -0300

    Connection to Mongodb Atlas added

---
+++ whatsapp-backend/server.js
@@ -0,0 +1,46 @@
+//importing
+import express from "express";
+import mongoose from "mongoose";
+import Messages from "./dbMessages.js";
+
+//app config
+const app = express();
+const port = process.env.PORT || 9000;
+
+//middleware
+app.use(express.json());
+
+//DB config
+const connection_url = 'mongodb+srv://SolohagaLukas:uHMC1ussp1ryggrI@cluster0.nzuoh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+mongoose.connect(connection_url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+//api routes
+app.get('/', (req,res) => res.status(200).send('hello world'));
+
+app.get('/messages/sync', (req, res) => {
+    Messages.find((err, data) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.status(200).send(data);
+        }
+    });
+});
+
+app.post('/messages/new', (req, res) => {
+    const dbMessage = req.body;
+
+    Messages.create(dbMessage, (err, data) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.status(201).send(data);
+        }
+    });
+});
+
+//listen
+app.listen(port, () => console.log(`Listening on localhost:${port}`));
